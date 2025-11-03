b07f2f73f8b80300347cd5350a6d04b2c60064d3
Zakariyya <zakariyyazakariyyali@gmail.com>
2025-11-04 02:01:09 +0400

    Reorder casinos: Move Rizzio to second position
    - Rizzio now second after SpinMillion
    - GoldenGenie and Qbet reordered below
    - Updated casino listing order

--- src/config/site.ts
+++ src/config/site.ts
@@ -31,18 +31,11 @@ export const siteConfig = {
       playLink: "https://www.spinmillionpartners.com/visit/?bta=35560&nci=5363&utm_campaign=SpinMillion-CXUK01&afp="
     },
     {
-      name: "GoldenGenie",
-      logo: "goldengenie.webp", // Change this filename to switch logos
-      bonus: "100 Free spins + 400% up to 2000€",
-      rating: 9.8,
-      playLink: "https://go.goldenwinpartners.com/visit/?bta=35642&nci=5468&utm_campaign=GoldenGenie-CXUK01&afp="
-    },
-    {
-      name: "Qbet",
-      logo: "qbet.png", // Change this filename to switch logos
-      bonus: "Bonus up to £100 + 50 Free Spins ",
-      rating: 9.6,
-      playLink: "https://record.betgroup.partners/_YnRh4Sd1bK99cPJOz4tFzmNd7ZgqdRLk/2/?pg=5&s2s.req_id=QBET-CXUK01&payload="
+      name: "Rizzio",
+      logo: "rizzio-logo-nl.svg", // Change this filename to switch logos
+      bonus: "EXCLUSIVE WELCOME CASINO BONUS 250% UP TO £2.500 + 100 FS ON WILDS OF FORTUNE!",
+      rating: 9.0,
+      playLink: "https://go.infinitystarspartners.com/visit/?bta=39161&nci=8800&utm_campaign=Rizzio-CXUK01&afp="
     },
     {
       name: "LuckyTwice",
@@ -53,12 +46,21 @@ export const siteConfig = {
     }
     ,
     {
-      name: "Rizzio",
-      logo: "rizzio-logo-nl.svg", // Change this filename to switch logos
-      bonus: "EXCLUSIVE WELCOME CASINO BONUS 250% UP TO £2.500 + 100 FS ON WILDS OF FORTUNE!",
-      rating: 9.0,
-      playLink: "https://go.infinitystarspartners.com/visit/?bta=39161&nci=8800&utm_campaign=Rizzio-CXUK01&afp="
+      name: "Qbet",
+      logo: "qbet.png", // Change this filename to switch logos
+      bonus: "Bonus up to £100 + 50 Free Spins ",
+      rating: 9.6,
+      playLink: "https://record.betgroup.partners/_YnRh4Sd1bK99cPJOz4tFzmNd7ZgqdRLk/2/?pg=5&s2s.req_id=QBET-CXUK01&payload="
     },
+    {
+      name: "GoldenGenie",
+      logo: "goldengenie.webp", // Change this filename to switch logos
+      bonus: "100 Free spins + 400% up to 2000€",
+      rating: 9.8,
+      playLink: "https://go.goldenwinpartners.com/visit/?bta=35642&nci=5468&utm_campaign=GoldenGenie-CXUK01&afp="
+    },
+    
+    
     {
       name: "FishAndSpins",
       logo: "fishandspins.svg", // Change this filename to switch logos
