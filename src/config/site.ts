a9bfbd78ddcac29048ccc5a2995e3aae82f43284
Zakariyya <zakariyyazakariyyali@gmail.com>
2025-11-04 02:56:36 +0400

    Auto-resequence casino ratings based on current order
    - LuckyTwice: 8.6 -> 9.6 (third position)
    - FishAndSpins: 8.4 -> 9.4 (fourth position)
    - Rizzio: 9.6 -> 9.2 (fifth position)
    - GoldenGenie: 9.4 -> 9.0 (sixth position)
    - GoldenLion: 9.2 -> 8.8 (seventh position)
    - WinBeast: 9.0 -> 8.6 (eighth position)
    - 50Crowns: 8.8 -> 8.4 (ninth position)
    - Ratings now follow descending sequence from 10.0 to 7.8

--- src/config/site.ts
+++ src/config/site.ts
@@ -38,45 +38,10 @@ export const siteConfig = {
       playLink: "https://www.spinmillionpartners.com/visit/?bta=35560&nci=5363&utm_campaign=SpinMillion-CXUK01&afp="
     },
     {
-      name: "Rizzio",
-      logo: "rizzio-logo-nl.svg", // Change this filename to switch logos
-      bonus: "EXCLUSIVE WELCOME CASINO BONUS 250% UP TO £2.500 + 100 FS ON WILDS OF FORTUNE!",
-      rating: 9.6,
-      playLink: "https://go.infinitystarspartners.com/visit/?bta=39161&nci=8800&utm_campaign=Rizzio-CXUK01&afp="
-    },
-    {
-      name: "GoldenGenie",
-      logo: "goldengenie.webp", // Change this filename to switch logos
-      bonus: "100 Free spins + 400% up to 2000€",
-      rating: 9.4,
-      playLink: "https://go.goldenwinpartners.com/visit/?bta=35642&nci=5468&utm_campaign=GoldenGenie-CXUK01&afp="
-    },
-    {
-      name: "GoldenLion",
-      logo: "goldenlion.png", // Change this filename to switch logos
-      bonus: "100 Free spins + 400% up to 2000€",
-      rating: 9.2,
-      playLink: "https://go.goldenwinpartners.com/visit/?bta=35642&nci=5349&utm_campaign=GoldenLion-CXUK01&afp="
-    },
-    {
-      name: "WinBeast",
-      logo: "winbeast.svg", // Change this filename to switch logos
-      bonus: "Welcome Pack €/£3000 + 300 FS",
-      rating: 9.0,
-      playLink: "https://beast-net.co/aojqgntf6?subid=WinBeast-CXUK01&visit_id="
-    },
-    {
-      name: "50Crowns",
-      logo: "50crowns.svg", // Change this filename to switch logos
-      bonus: "100% up to £500 + 100 FS",
-      rating: 8.8,
-      playLink: "https://beast-https://50crownsplay.com/je5suztrm?subid=50Crowns-CXUK01&clickid=.co/aojqgntf6?subid=WinBeast-CXUK01&visit_id="
-    },
-    {
       name: "LuckyTwice",
       logo: "luckytwice.webp", // Change this filename to switch logos
       bonus: "100% UP TO £500 + 250 FREE SPINS",
-      rating: 8.6,
+      rating: 9.6,
       playLink: "https://luckytwice.site/j90006c83?subid=LuckyTwice-CXUK01&clickid="
     }
     ,
@@ -84,9 +49,45 @@ export const siteConfig = {
       name: "FishAndSpins",
       logo: "fishandspins.svg", // Change this filename to switch logos
       bonus: "WELCOME BONUS UP TO €1500 PLUS 175 BONUS SPINS",
-      rating: 8.4,
+      rating: 9.4,
       playLink: "https://fishnspins.site/jce4ac4a7?subid=FishAndSpins-CXUK01&clickid="
     },
+    {
+      name: "Rizzio",
+      logo: "rizzio-logo-nl.svg", // Change this filename to switch logos
+      bonus: "EXCLUSIVE WELCOME CASINO BONUS 250% UP TO £2.500 + 100 FS ON WILDS OF FORTUNE!",
+      rating: 9.2,
+      playLink: "https://go.infinitystarspartners.com/visit/?bta=39161&nci=8800&utm_campaign=Rizzio-CXUK01&afp="
+    },
+    {
+      name: "GoldenGenie",
+      logo: "goldengenie.webp", // Change this filename to switch logos
+      bonus: "100 Free spins + 400% up to 2000€",
+      rating: 9.0,
+      playLink: "https://go.goldenwinpartners.com/visit/?bta=35642&nci=5468&utm_campaign=GoldenGenie-CXUK01&afp="
+    },
+    {
+      name: "GoldenLion",
+      logo: "goldenlion.png", // Change this filename to switch logos
+      bonus: "100 Free spins + 400% up to 2000€",
+      rating: 8.8,
+      playLink: "https://go.goldenwinpartners.com/visit/?bta=35642&nci=5349&utm_campaign=GoldenLion-CXUK01&afp="
+    },
+    {
+      name: "WinBeast",
+      logo: "winbeast.svg", // Change this filename to switch logos
+      bonus: "Welcome Pack €/£3000 + 300 FS",
+      rating: 8.6,
+      playLink: "https://beast-net.co/aojqgntf6?subid=WinBeast-CXUK01&visit_id="
+    },
+    {
+      name: "50Crowns",
+      logo: "50crowns.svg", // Change this filename to switch logos
+      bonus: "100% up to £500 + 100 FS",
+      rating: 8.4,
+      playLink: "https://beast-https://50crownsplay.com/je5suztrm?subid=50Crowns-CXUK01&clickid=.co/aojqgntf6?subid=WinBeast-CXUK01&visit_id="
+    },
+    
     
     {
       name: "UspinMe",
