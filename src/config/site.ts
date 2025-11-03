916b2d092c37b92939d0e1fa8fd393756d130baf
Zakariyya <zakariyyazakariyyali@gmail.com>
2025-11-04 03:31:05 +0400

    Comment out most casinos, only GrandIvy active
    - Commented out Qbet, SpinMillion, GoldenGenie, Rizzio, FishAndSpins, WinBeast, LuckyTwice, GoldenLion, 50Crowns, UspinMe, MadCasino, DraculaCasino
    - Only GrandIvy remains active in the casino list

--- src/config/site.ts
+++ src/config/site.ts
@@ -23,104 +23,104 @@ export const siteConfig = {
   
   // Casino brands data
   casinos: [
-    {
-      name: "Qbet",
-      logo: "qbet.png", // Change this filename to switch logos
-      bonus: "Bonus up to £100 + 50 Free Spins ",
-      rating: 10.0,
-      playLink: "https://record.betgroup.partners/_YnRh4Sd1bK99cPJOz4tFzmNd7ZgqdRLk/2/?pg=5&s2s.req_id=QBET-CXUK01&payload="
-    },
-    {
-      name: "SpinMillion",
-      logo: "spinmillion.webp", // Change this filename to switch logos
-      bonus: "100% up to £750 + 100 Free Spins + 3x Wheel of Luck",
-      rating: 9.8,
-      playLink: "https://www.spinmillionpartners.com/visit/?bta=35560&nci=5363&utm_campaign=SpinMillion-CXUK01&afp="
-    },
-    {
-      name: "GoldenGenie",
-      logo: "goldengenie.webp", // Change this filename to switch logos
-      bonus: "100 Free spins + 400% up to 2000€",
-      rating: 9.6,
-      playLink: "https://go.goldenwinpartners.com/visit/?bta=35642&nci=5468&utm_campaign=GoldenGenie-CXUK01&afp="
-    },
-    {
-      name: "Rizzio",
-      logo: "rizzio-logo-nl.svg", // Change this filename to switch logos
-      bonus: "EXCLUSIVE WELCOME CASINO BONUS 250% UP TO £2.500 + 100 FS ON WILDS OF FORTUNE!",
-      rating: 9.4,
-      playLink: "https://go.infinitystarspartners.com/visit/?bta=39161&nci=8800&utm_campaign=Rizzio-CXUK01&afp="
-    },
+    // {
+    //   name: "Qbet",
+    //   logo: "qbet.png", // Change this filename to switch logos
+    //   bonus: "Bonus up to £100 + 50 Free Spins ",
+    //   rating: 10.0,
+    //   playLink: "https://record.betgroup.partners/_YnRh4Sd1bK99cPJOz4tFzmNd7ZgqdRLk/2/?pg=5&s2s.req_id=QBET-CXUK01&payload="
+    // },
+    // {
+    //   name: "SpinMillion",
+    //   logo: "spinmillion.webp", // Change this filename to switch logos
+    //   bonus: "100% up to £750 + 100 Free Spins + 3x Wheel of Luck",
+    //   rating: 9.8,
+    //   playLink: "https://www.spinmillionpartners.com/visit/?bta=35560&nci=5363&utm_campaign=SpinMillion-CXUK01&afp="
+    // },
+    // {
+    //   name: "GoldenGenie",
+    //   logo: "goldengenie.webp", // Change this filename to switch logos
+    //   bonus: "100 Free spins + 400% up to 2000€",
+    //   rating: 9.6,
+    //   playLink: "https://go.goldenwinpartners.com/visit/?bta=35642&nci=5468&utm_campaign=GoldenGenie-CXUK01&afp="
+    // },
+    // {
+    //   name: "Rizzio",
+    //   logo: "rizzio-logo-nl.svg", // Change this filename to switch logos
+    //   bonus: "EXCLUSIVE WELCOME CASINO BONUS 250% UP TO £2.500 + 100 FS ON WILDS OF FORTUNE!",
+    //   rating: 9.4,
+    //   playLink: "https://go.infinitystarspartners.com/visit/?bta=39161&nci=8800&utm_campaign=Rizzio-CXUK01&afp="
+    // },
     
-    {
-      name: "FishAndSpins",
-      logo: "fishandspins.svg", // Change this filename to switch logos
-      bonus: "WELCOME BONUS UP TO €1500 PLUS 175 BONUS SPINS",
-      rating: 9.2,
-      playLink: "https://fishnspins.site/jce4ac4a7?subid=FishAndSpins-CXUK01&clickid="
-    },
-    {
-      name: "WinBeast",
-      logo: "winbeast.svg", // Change this filename to switch logos
-      bonus: "Welcome Pack €/£3000 + 300 FS",
-      rating: 9.0,
-      playLink: "https://beast-net.co/aojqgntf6?subid=WinBeast-CXUK01&visit_id="
-    },
-    {
-      name: "LuckyTwice",
-      logo: "luckytwice.webp", // Change this filename to switch logos
-      bonus: "100% UP TO £500 + 250 FREE SPINS",
-      rating: 8.8,
-      playLink: "https://luckytwice.site/j90006c83?subid=LuckyTwice-CXUK01&clickid="
-    }
-    ,
-    {
-      name: "GoldenLion",
-      logo: "goldenlion.png", // Change this filename to switch logos
-      bonus: "100 Free spins + 400% up to 2000€",
-      rating: 8.6,
-      playLink: "https://go.goldenwinpartners.com/visit/?bta=35642&nci=5349&utm_campaign=GoldenLion-CXUK01&afp="
-    },
-    {
-      name: "50Crowns",
-      logo: "50crowns.svg", // Change this filename to switch logos
-      bonus: "100% up to £500 + 100 FS",
-      rating: 8.4,
-      playLink: "https://beast-https://50crownsplay.com/je5suztrm?subid=50Crowns-CXUK01&clickid=.co/aojqgntf6?subid=WinBeast-CXUK01&visit_id="
-    },
-    
-    
-    {
-      name: "UspinMe",
-      logo: "uspinme.png", // Change this filename to switch logos
-      bonus: "CASINO WELCOME BONUS Up to €5,000 + 350 Free Spins",
-      rating: 8.2,
-      playLink: "https://record.primewebaffiliates.com/_nCRCtj1NPnffkb3gg_bGdGNd7ZgqdRLk/1/&s2s.req_id=UspinMe-CXUK01&payload="
-    },
-    {
-      name: "MadCasino",
-      logo: "MADCASINO.png", // Change this filename to switch logos
-      bonus: "Welcome offer: 777% up to $11,250 + 20% cashbcak",
-      rating: 8.0,
-      playLink: "https://go.afftrackio.com/visit/?bta=35684&nci=5354&utm_campaign=MadCasino-CXUK01&afp="
-    },
-
-    {
-      name: "DraculaCasino",
-      logo: "dracula.png", // Change this filename to switch logos
-      bonus: "Slots welcome Package 777% up to $11,665.50 + 20% cashback Wager: 25x",
-      rating: 7.8,
-      playLink: "https://go.afftrackio.com/visit/?bta=35684&nci=5362&utm_campaign=DraculaCasino-CXUK01&afp="
-    }
+    // {
+    //   name: "FishAndSpins",
+    //   logo: "fishandspins.svg", // Change this filename to switch logos
+    //   bonus: "WELCOME BONUS UP TO €1500 PLUS 175 BONUS SPINS",
+    //   rating: 9.2,
+    //   playLink: "https://fishnspins.site/jce4ac4a7?subid=FishAndSpins-CXUK01&clickid="
+    // },
+    // {
+    //   name: "WinBeast",
+    //   logo: "winbeast.svg", // Change this filename to switch logos
+    //   bonus: "Welcome Pack €/£3000 + 300 FS",
+    //   rating: 9.0,
+    //   playLink: "https://beast-net.co/aojqgntf6?subid=WinBeast-CXUK01&visit_id="
+    // },
+    // {
+    //   name: "LuckyTwice",
+    //   logo: "luckytwice.webp", // Change this filename to switch logos
+    //   bonus: "100% UP TO £500 + 250 FREE SPINS",
+    //   rating: 8.8,
+    //   playLink: "https://luckytwice.site/j90006c83?subid=LuckyTwice-CXUK01&clickid="
+    // }
     // ,
+    // {
+    //   name: "GoldenLion",
+    //   logo: "goldenlion.png", // Change this filename to switch logos
+    //   bonus: "100 Free spins + 400% up to 2000€",
+    //   rating: 8.6,
+    //   playLink: "https://go.goldenwinpartners.com/visit/?bta=35642&nci=5349&utm_campaign=GoldenLion-CXUK01&afp="
+    // },
+    // {
+    //   name: "50Crowns",
+    //   logo: "50crowns.svg", // Change this filename to switch logos
+    //   bonus: "100% up to £500 + 100 FS",
+    //   rating: 8.4,
+    //   playLink: "https://beast-https://50crownsplay.com/je5suztrm?subid=50Crowns-CXUK01&clickid=.co/aojqgntf6?subid=WinBeast-CXUK01&visit_id="
+    // },
+    
+    
+    // {
+    //   name: "UspinMe",
+    //   logo: "uspinme.png", // Change this filename to switch logos
+    //   bonus: "CASINO WELCOME BONUS Up to €5,000 + 350 Free Spins",
+    //   rating: 8.2,
+    //   playLink: "https://record.primewebaffiliates.com/_nCRCtj1NPnffkb3gg_bGdGNd7ZgqdRLk/1/&s2s.req_id=UspinMe-CXUK01&payload="
+    // },
+    // {
+    //   name: "MadCasino",
+    //   logo: "MADCASINO.png", // Change this filename to switch logos
+    //   bonus: "Welcome offer: 777% up to $11,250 + 20% cashbcak",
+    //   rating: 8.0,
+    //   playLink: "https://go.afftrackio.com/visit/?bta=35684&nci=5354&utm_campaign=MadCasino-CXUK01&afp="
+    // },
 
     // {
-    //   name: "GrandIvy",
-    //   logo: "grandivy.webp", // Change this filename to switch logos
-    //   bonus: "WELCOME BONUS UP TO €1500 PLUS 175 BONUS SPINS",
-    //   rating: 8.2,
-    //   playLink: "https://media1.casimbaaff.com/redirect.aspx?pid=16629&lpid=224&bid=1633&subid=GrandIvy-CXUK01&clickid="
+    //   name: "DraculaCasino",
+    //   logo: "dracula.png", // Change this filename to switch logos
+    //   bonus: "Slots welcome Package 777% up to $11,665.50 + 20% cashback Wager: 25x",
+    //   rating: 7.8,
+    //   playLink: "https://go.afftrackio.com/visit/?bta=35684&nci=5362&utm_campaign=DraculaCasino-CXUK01&afp="
     // }
+    // ,
+
+    {
+      name: "GrandIvy",
+      logo: "grandivy.webp", // Change this filename to switch logos
+      bonus: "WELCOME BONUS UP TO €1500 PLUS 175 BONUS SPINS",
+      rating: 8.2,
+      playLink: "https://media1.casimbaaff.com/redirect.aspx?pid=16629&lpid=224&bid=1633&subid=GrandIvy-CXUK01&clickid="
+    }
   ],
 
   
