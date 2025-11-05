490c55b33e5e7ebaad87046578e11fea4731b9a3
Zakariyya <zakariyyazakariyyali@gmail.com>
2025-11-06 02:19:06 +0400

    Move SpinMillion to 3rd position and update ratings (10.0 to 8.0)

--- src/config/site.ts
+++ src/config/site.ts
@@ -24,28 +24,28 @@ export const siteConfig = {
   // Casino brands data
   casinos: [
      
-    {
-      name: "SpinMillion",
-      logo: "spinmillion.webp", // Change this filename to switch logos
-      bonus: "100% up to £750\n100 Free Spins\n3x Wheel of Luck",
-      rating: 10.0,
-      playLink: "https://www.spinmillionpartners.com/visit/?bta=35560&nci=5363&utm_campaign=SpinMillion-CXUK01&afp="
-    },
+   
     {
       name: "LuckyTwice",
       logo: "luckytwice.webp", // Change this filename to switch logos
       bonus: "100% UP TO £500\n250 FREE SPINS",
-      rating: 9.8,
+      rating: 10.0,
       playLink: "https://luckytwice.site/j90006c83?subid=LuckyTwice-CXUK01&clickid="
     },
     {
       name: "Qbet",
       logo: "qbet.png", // Change this filename to switch logos
       bonus: "Bonus up to £100\n50 Free Spins ",
-      rating: 9.6,
+      rating: 9.8,
       playLink: "https://record.betgroup.partners/_YnRh4Sd1bK99cPJOz4tFzmNd7ZgqdRLk/2/?pg=5&s2s.req_id=QBET-CXUK01&payload="
     },
-  
+    {
+      name: "SpinMillion",
+      logo: "spinmillion.webp", // Change this filename to switch logos
+      bonus: "100% up to £750\n100 Free Spins\n3x Wheel of Luck",
+      rating: 9.6,
+      playLink: "https://www.spinmillionpartners.com/visit/?bta=35560&nci=5363&utm_campaign=SpinMillion-CXUK01&afp="
+    },
     {
       name: "FishAndSpins",
       logo: "fishandspins.svg", // Change this filename to switch logos
