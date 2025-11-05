9894a7fb4f9077f3bed12d9672b1f6be43965457
Zakariyya <zakariyyazakariyyali@gmail.com>
2025-11-05 16:36:07 +0400

    Clean up bonus text formatting - remove plus signs

--- src/config/site.ts
+++ src/config/site.ts
@@ -27,21 +27,21 @@ export const siteConfig = {
     {
       name: "LuckyTwice",
       logo: "luckytwice.webp", // Change this filename to switch logos
-      bonus: "100% UP TO £500\n+\n250 FREE SPINS",
+      bonus: "100% UP TO £500\n250 FREE SPINS",
       rating: 10.0,
       playLink: "https://luckytwice.site/j90006c83?subid=LuckyTwice-CXUK01&clickid="
     },
       {
       name: "Qbet",
       logo: "qbet.png", // Change this filename to switch logos
-      bonus: "Bonus up to £100 + 50 Free Spins ",
+      bonus: "Bonus up to £100\n50 Free Spins ",
       rating: 9.8,
       playLink: "https://record.betgroup.partners/_YnRh4Sd1bK99cPJOz4tFzmNd7ZgqdRLk/2/?pg=5&s2s.req_id=QBET-CXUK01&payload="
     },
     {
       name: "SpinMillion",
       logo: "spinmillion.webp", // Change this filename to switch logos
-      bonus: "100% up to £750\n+\n100 Free Spins\n+\n3x Wheel of Luck",
+      bonus: "100% up to £750\n100 Free Spins\n3x Wheel of Luck",
       rating: 9.6,
       playLink: "https://www.spinmillionpartners.com/visit/?bta=35560&nci=5363&utm_campaign=SpinMillion-CXUK01&afp="
     },
@@ -56,7 +56,7 @@ export const siteConfig = {
     {
       name: "WinBeast",
       logo: "winbeast.svg", // Change this filename to switch logos
-      bonus: "Welcome Pack €/£3000\n+\n300 FS",
+      bonus: "Welcome Pack €/£3000\n300 FS",
       rating: 9.2,
       playLink: "https://beast-net.co/aue1i1msc?subid=WinBeast-CXUK01&visit_id="
     }
@@ -64,7 +64,7 @@ export const siteConfig = {
     {
         name: "WagerTales",
         logo: "wagertales.png", // Change this filename to switch logos
-        bonus: "WELCOME BONUS\n6000£\n+\n600\nFREE SPINS",
+        bonus: "WELCOME BONUS\n6000£\n600\nFREE SPINS",
         rating: 9.0,
         playLink: "https://go.magicwinpartners.com/visit/?bta=35532&nci=5434&utm_campaign=WagerTales-CXUK01&afp="
       },
@@ -72,14 +72,14 @@ export const siteConfig = {
     {
       name: "GoldenGenie",
       logo: "goldengenie.webp", // Change this filename to switch logos
-      bonus: "100 Free spins\n+\n400% up to 2000€",
+      bonus: "100 Free spins\n400% up to 2000€",
       rating: 8.8,
       playLink: "https://go.goldenwinpartners.com/visit/?bta=35642&nci=5468&utm_campaign=GoldenGenie-CXUK01&afp="
     },
     {
       name: "Rizzio",
       logo: "rizzio-logo-nl.svg", // Change this filename to switch logos
-      bonus: "EXCLUSIVE WELCOME CASINO BONUS\n250% UP TO £2.500\n+\n100 FS ON WILDS OF FORTUNE!",
+      bonus: "EXCLUSIVE WELCOME CASINO BONUS\n250% UP TO £2.500\n100 FS ON WILDS OF FORTUNE!",
       rating: 8.6,
       playLink: "https://go.infinitystarspartners.com/visit/?bta=39161&nci=8800&utm_campaign=Rizzio-CXUK01&afp="
     },
@@ -88,14 +88,14 @@ export const siteConfig = {
     {
       name: "GoldenLion",
       logo: "goldenlion.png", // Change this filename to switch logos
-      bonus: "100 Free spins\n+\n400% up to 2000€",
+      bonus: "100 Free spins\n400% up to 2000€",
       rating: 8.4,
       playLink: "https://go.goldenwinpartners.com/visit/?bta=35642&nci=5349&utm_campaign=GoldenLion-CXUK01&afp="
     },
     {
       name: "50Crowns",
       logo: "50crowns.svg", // Change this filename to switch logos
-      bonus: "100% up to £500\n+\n100 FS",
+      bonus: "100% up to £500\n100 FS",
       rating: 8.2,
       playLink: "https://beast-https://50crownsplay.com/je5suztrm?subid=50Crowns-CXUK01&clickid=.co/aojqgntf6?subid=WinBeast-CXUK01&visit_id="
     },
@@ -103,7 +103,7 @@ export const siteConfig = {
     {
       name: "MadCasino",
       logo: "MADCASINO.png", // Change this filename to switch logos
-      bonus: "Welcome offer: 777% up to £$11,250\n+\n20% cashbcak",
+      bonus: "Welcome offer: 777% up to £$11,250\n20% cashbcak",
       rating: 8.0,
       playLink: "https://go.afftrackio.com/visit/?bta=35684&nci=5354&utm_campaign=MadCasino-CXUK01&afp="
     },
@@ -111,7 +111,7 @@ export const siteConfig = {
     {
       name: "DraculaCasino",
       logo: "dracula.png", // Change this filename to switch logos
-      bonus: "Slots welcome Package 777% \nup to $11,665.50\n+\n20% cashback Wager: 25x",
+      bonus: "Slots welcome Package 777% \nup to $11,665.50\n20% cashback Wager: 25x",
       rating: 7.8,
       playLink: "https://go.afftrackio.com/visit/?bta=35684&nci=5362&utm_campaign=DraculaCasino-CXUK01&afp="
     }
