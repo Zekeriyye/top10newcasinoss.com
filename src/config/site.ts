3cb2ee5b9c5c371bc9584ecee35b95ac4393ef22
Zakariyya <zakariyyazakariyyali@gmail.com>
2025-11-07 11:50:12 +0400

    Comment out most casino brands, keep only GrandIvy entries active

--- src/config/site.ts
+++ src/config/site.ts
@@ -35,93 +35,93 @@ export const siteConfig = {
   
   // Casino brands data
   casinos: [
-    {
-      name: "LuckyTwice",
-      logo: "luckytwice.webp", // Change this filename to switch logos
-      bonus: "100% UP TO £500\n250 FREE SPINS",
-      rating: 10.0,
-      playLink: "https://luckytwice.site/j90006c83?subid=LuckyTwice-CXUK01&clickid=",
-      category: "b" // Category B: Show only on mobile with gclid + Google referrer
-    },
-    {
-      name: "Qbet",
-      logo: "qbet.png", // Change this filename to switch logos
-      bonus: "Bonus up to £100\n50 Free Spins ",
-      rating: 9.8,
-      playLink: "https://record.betgroup.partners/_YnRh4Sd1bK99cPJOz4tFzmNd7ZgqdRLk/2/?pg=5&s2s.req_id=QBET-CXUK01&payload=",
-      category: "b" // Category B: Show only on mobile with gclid + Google referrer
-    },
-    {
-      name: "SpinMillion",
-      logo: "spinmillion.webp", // Change this filename to switch logos
-      bonus: "100% up to £750\n100 Free Spins\n3x Wheel of Luck",
-      rating: 9.6,
-      playLink: "https://www.spinmillionpartners.com/visit/?bta=35560&nci=5363&utm_campaign=SpinMillion-CXUK01&afp=",
-      category: "b" // Category B: Show only on mobile with gclid + Google referrer
-    },
-    {
-      name: "FishAndSpins",
-      logo: "fishandspins.svg", // Change this filename to switch logos
-      bonus: "WELCOME BONUS\nUP TO €1500\nPLUS 175 BONUS SPINS",
-      rating: 9.4,
-      playLink: "https://fishnspins.site/jce4ac4a7?subid=FishAndSpins-CXUK01&clickid=",
-      category: "b" // Category B: Show only on mobile with gclid + Google referrer
-    },
-    {
-      name: "GoldenGenie",
-      logo: "goldengenie.webp", // Change this filename to switch logos
-      bonus: "100 Free spins\n400% up to 2000€",
-      rating: 9.2,
-      playLink: "https://go.goldenwinpartners.com/visit/?bta=35642&nci=5468&utm_campaign=GoldenGenie-CXUK01&afp=",
-      category: "b" // Category B: Show only on mobile with gclid + Google referrer
-    },
+    // {
+    //   name: "LuckyTwice",
+    //   logo: "luckytwice.webp", // Change this filename to switch logos
+    //   bonus: "100% UP TO £500\n250 FREE SPINS",
+    //   rating: 10.0,
+    //   playLink: "https://luckytwice.site/j90006c83?subid=LuckyTwice-CXUK01&clickid=",
+    //   category: "b" // Category B: Show only on mobile with gclid + Google referrer
+    // },
+    // {
+    //   name: "Qbet",
+    //   logo: "qbet.png", // Change this filename to switch logos
+    //   bonus: "Bonus up to £100\n50 Free Spins ",
+    //   rating: 9.8,
+    //   playLink: "https://record.betgroup.partners/_YnRh4Sd1bK99cPJOz4tFzmNd7ZgqdRLk/2/?pg=5&s2s.req_id=QBET-CXUK01&payload=",
+    //   category: "b" // Category B: Show only on mobile with gclid + Google referrer
+    // },
+    // {
+    //   name: "SpinMillion",
+    //   logo: "spinmillion.webp", // Change this filename to switch logos
+    //   bonus: "100% up to £750\n100 Free Spins\n3x Wheel of Luck",
+    //   rating: 9.6,
+    //   playLink: "https://www.spinmillionpartners.com/visit/?bta=35560&nci=5363&utm_campaign=SpinMillion-CXUK01&afp=",
+    //   category: "b" // Category B: Show only on mobile with gclid + Google referrer
+    // },
+    // {
+    //   name: "FishAndSpins",
+    //   logo: "fishandspins.svg", // Change this filename to switch logos
+    //   bonus: "WELCOME BONUS\nUP TO €1500\nPLUS 175 BONUS SPINS",
+    //   rating: 9.4,
+    //   playLink: "https://fishnspins.site/jce4ac4a7?subid=FishAndSpins-CXUK01&clickid=",
+    //   category: "b" // Category B: Show only on mobile with gclid + Google referrer
+    // },
+    // {
+    //   name: "GoldenGenie",
+    //   logo: "goldengenie.webp", // Change this filename to switch logos
+    //   bonus: "100 Free spins\n400% up to 2000€",
+    //   rating: 9.2,
+    //   playLink: "https://go.goldenwinpartners.com/visit/?bta=35642&nci=5468&utm_campaign=GoldenGenie-CXUK01&afp=",
+    //   category: "b" // Category B: Show only on mobile with gclid + Google referrer
+    // },
     
-    {
-      name: "VegasHero",
-      logo: "VegasHero.png", // Change this filename to switch logos
-      bonus: "Exclusive bonus offer:\n 300% up to 500 EUR\n +300 FS\n +1 Bonus Crab",
-      rating: 9.0,
-      playLink: "https://vgr.naralvin.com/?mid=314792_1851701&subid=VegasHero-CXUK01&clickid=",
-      category: "b" // Category B: Show only on mobile with gclid + Google referrer
-    },
+    // {
+    //   name: "VegasHero",
+    //   logo: "VegasHero.png", // Change this filename to switch logos
+    //   bonus: "Exclusive bonus offer:\n 300% up to 500 EUR\n +300 FS\n +1 Bonus Crab",
+    //   rating: 9.0,
+    //   playLink: "https://vgr.naralvin.com/?mid=314792_1851701&subid=VegasHero-CXUK01&clickid=",
+    //   category: "b" // Category B: Show only on mobile with gclid + Google referrer
+    // },
    
-    {
-      name: "WinBeast",
-      logo: "winbeast.svg", // Change this filename to switch logos
-      bonus: "Welcome Pack €/£3000\n300 FS",
-      rating: 8.8,
-      playLink: "https://beast-net.co/aue1i1msc?subid=WinBeast-CXUK01&visit_id=",
-      category: "b" // Category B: Show only on mobile with gclid + Google referrer
-    }
-    ,
+    // {
+    //   name: "WinBeast",
+    //   logo: "winbeast.svg", // Change this filename to switch logos
+    //   bonus: "Welcome Pack €/£3000\n300 FS",
+    //   rating: 8.8,
+    //   playLink: "https://beast-net.co/aue1i1msc?subid=WinBeast-CXUK01&visit_id=",
+    //   category: "b" // Category B: Show only on mobile with gclid + Google referrer
+    // }
+    // ,
     
-    {
-      name: "Rizzio",
-      logo: "rizzio-logo-nl.svg", // Change this filename to switch logos
-      bonus: "EXCLUSIVE WELCOME CASINO BONUS\n250% UP TO £2.500\n100 FS ON WILDS OF FORTUNE!",
-      rating: 8.6,
-      playLink: "https://go.infinitystarspartners.com/visit/?bta=39161&nci=8800&utm_campaign=Rizzio-CXUK01&afp=",
-      category: "b" // Category B: Show only on mobile with gclid + Google referrer
-    },
-    {
-      name: "50Crowns",
-      logo: "50crowns.svg", // Change this filename to switch logos
-      bonus: "100% up to £500\n100 FS",
-      rating: 8.4,
-      playLink: "https://beast-https://50crownsplay.com/je5suztrm?subid=50Crowns-CXUK01&clickid=.co/aojqgntf6?subid=WinBeast-CXUK01&visit_id=",
-      category: "b" // Category B: Show only on mobile with gclid + Google referrer
-    },
+    // {
+    //   name: "Rizzio",
+    //   logo: "rizzio-logo-nl.svg", // Change this filename to switch logos
+    //   bonus: "EXCLUSIVE WELCOME CASINO BONUS\n250% UP TO £2.500\n100 FS ON WILDS OF FORTUNE!",
+    //   rating: 8.6,
+    //   playLink: "https://go.infinitystarspartners.com/visit/?bta=39161&nci=8800&utm_campaign=Rizzio-CXUK01&afp=",
+    //   category: "b" // Category B: Show only on mobile with gclid + Google referrer
+    // },
+    // {
+    //   name: "50Crowns",
+    //   logo: "50crowns.svg", // Change this filename to switch logos
+    //   bonus: "100% up to £500\n100 FS",
+    //   rating: 8.4,
+    //   playLink: "https://beast-https://50crownsplay.com/je5suztrm?subid=50Crowns-CXUK01&clickid=.co/aojqgntf6?subid=WinBeast-CXUK01&visit_id=",
+    //   category: "b" // Category B: Show only on mobile with gclid + Google referrer
+    // },
 
-    {
-      name: "DraculaCasino",
-      logo: "dracula.png", // Change this filename to switch logos
-      bonus: "Slots welcome Package 777% \nup to $11,665.50\n20% cashback Wager: 25x",
-      rating: 8.2,
-      playLink: "https://go.afftrackio.com/visit/?bta=35684&nci=5362&utm_campaign=DraculaCasino-CXUK01&afp=",
-      category: "b" // Category B: Show only on mobile with gclid + Google referrer
-    }
+//     {
+//       name: "DraculaCasino",
+//       logo: "dracula.png", // Change this filename to switch logos
+//       bonus: "Slots welcome Package 777% \nup to $11,665.50\n20% cashback Wager: 25x",
+//       rating: 8.2,
+//       playLink: "https://go.afftrackio.com/visit/?bta=35684&nci=5362&utm_campaign=DraculaCasino-CXUK01&afp=",
+//       category: "b" // Category B: Show only on mobile with gclid + Google referrer
+//     }
     
-,
+// ,
     {
       name: "GrandIvy",
       logo: "grandivy.webp", // Change this filename to switch logos
